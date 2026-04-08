68617cc9d8e228849e1c40758e8ec1d24a21b6ac
Vedant Tawde <vedanttawde.work@gmail.com>
2026-04-08 21:35:01 +0530

    Add server app entrypoint for validator

---
+++ server/app.py
@@ -0,0 +1,14 @@
+import os
+
+import uvicorn
+
+from my_env import app
+
+
+def main():
+    port = int(os.environ.get("PORT", "8080"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
+
+if __name__ == "__main__":
+    main()
